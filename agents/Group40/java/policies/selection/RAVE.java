6859a2eb7dd3f0628ad42dac2c9676168cfbe7d3
Egor Chernyshev <egorch.formal@gmail.com>
2023-12-13 23:42:27 +0000

    remove rave factor

--- agents/Group40/java/policies/selection/RAVE.java
+++ agents/Group40/java/policies/selection/RAVE.java
@@ -5,11 +5,9 @@ import javaV.MCTSNode;
 import javaV.common.RAVEStat;
 
 public class RAVE {
-    public static int cutoffFactor = 7;
-    public static int V = (MCTSAgent.simulationsCntPerCore * MCTSAgent.cores) * cutoffFactor;
+
     public static double calculateValue(MCTSNode child, MCTSNode parent){
         final RAVEStat raveStat = parent.RAVEStats[child.move.y][child.move.x];
-        final double raveFactor = Math.max(0, ((double)(V - child.N) / V));
-        return ( ((1 - raveFactor) * UCT.calculateValue(child, parent)) + (raveFactor * ((double) raveStat.Q / raveStat.N)) );
+        return (( UCT.calculateValue(child, parent) ) + ( ((double) raveStat.Q / raveStat.N ) ));
     }
 }
