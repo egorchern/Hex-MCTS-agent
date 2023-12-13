d100894611d6454c05dedaa7f0a8e74217e9a4d6
Egor Chernyshev <egorch.formal@gmail.com>
2023-12-13 01:31:13 +0000

    fix bug

--- agents/Group40/java/policies/selection/RAVE.java
+++ agents/Group40/java/policies/selection/RAVE.java
@@ -5,11 +5,11 @@ import javaV.MCTSNode;
 import javaV.common.RAVEStat;
 
 public class RAVE {
-    public static int cutoffFactor = 6;
+    public static int cutoffFactor = 5;
     public static int V = (MCTSAgent.simulationsCntPerCore * MCTSAgent.cores) * cutoffFactor;
-    public static double calculateValue(MCTSNode node, MCTSNode parent){
-        final RAVEStat raveStat = parent.RAVEStats[node.move.y][node.move.x];
-        final double raveFactor = Math.max(0, ((double)(V - raveStat.N) / V));
-        return ( ((1 - raveFactor) * UCT.calculateValue(node, parent)) + (raveFactor * ((double) raveStat.Q / raveStat.N)) );
+    public static double calculateValue(MCTSNode child, MCTSNode parent){
+        final RAVEStat raveStat = parent.RAVEStats[child.move.y][child.move.x];
+        final double raveFactor = Math.max(0, ((double)(V - child.N) / V));
+        return ( ((1 - raveFactor) * UCT.calculateValue(child, parent)) + (raveFactor * ((double) raveStat.Q / raveStat.N)) );
     }
 }
